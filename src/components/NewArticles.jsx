a2c09d7ce0860efcdf01958f4822a35163306c95
Venus <venusyip@outlook.com>
2025-03-27 22:10:34 +0000

    Add markup for new articles section

--- src/components/NewArticles.jsx
+++ src/components/NewArticles.jsx
@@ -1,1 +1,22 @@
-export default function NewArticles() {}
+export default function NewArticles() {
+  return (
+    <section className='new-articles'>
+      <h2 className='new-articles__main-heading'>New</h2>
+
+      <section className='new-articles__article'>
+        <h3 className='new-articles__article-heading'></h3>
+        <p className='new-articles__description'></p>
+      </section>
+
+      <section className='new-articles__article'>
+        <h3 className='new-articles__article-heading'></h3>
+        <p className='new-articles__description'></p>
+      </section>
+
+      <section className='new-articles__article'>
+        <h3 className='new-articles__article-heading'></h3>
+        <p className='new-articles__description'></p>
+      </section>
+    </section>
+  );
+}
