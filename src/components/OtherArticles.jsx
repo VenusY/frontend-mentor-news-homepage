0d079fe0590f7b4248c4ee504f3d8af3f0493a10
Venus <venusyip@outlook.com>
2025-03-27 22:35:09 +0000

    Add markup for other articles section

--- src/components/OtherArticles.jsx
+++ src/components/OtherArticles.jsx
@@ -1,1 +1,47 @@
-export default function OtherArticles() {}
+export default function OtherArticles() {
+  return (
+    <div className='other-articles'>
+      <article className='other-articles__article'>
+        <img
+          src={require('../assets/images/image-retro-pcs.jpg')}
+          alt='Retro PC'
+          className='other-articles__image'
+        />
+
+        <section className='other-articles__text-section'>
+          <h2 className='other-articles__number'></h2>
+          <h3 className='other-articles__article-heading'></h3>
+          <p className='other-articles__description'></p>
+        </section>
+      </article>
+
+      <article className='other-articles__article'>
+        <img
+          src={require('../assets/images/image-top-laptops.jpg')}
+          alt='Laptop keyboard close up'
+          className='other-articles__image'
+        />
+
+        <section className='other-articles__text-section'>
+          <h2 className='other-articles__number'></h2>
+          <h3 className='other-articles__article-heading'></h3>
+          <p className='other-articles__description'></p>
+        </section>
+      </article>
+
+      <article className='other-articles__article'>
+        <img
+          src={require('../assets/images/image-gaming-growth.jpg')}
+          alt='Game controller hovering above hand'
+          className='other-articles__image'
+        />
+
+        <section className='other-articles__text-section'>
+          <h2 className='other-articles__number'></h2>
+          <h3 className='other-articles__article-heading'></h3>
+          <p className='other-articles__description'></p>
+        </section>
+      </article>
+    </div>
+  );
+}
