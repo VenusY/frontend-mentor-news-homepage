f36e63ecf582a3be55aa10ac61e2a95f60e8600f
Venus <venusyip@outlook.com>
2025-03-27 22:07:51 +0000

    Add markup for main article

--- src/components/MainArticle.jsx
+++ src/components/MainArticle.jsx
@@ -1,1 +1,20 @@
-export default function MainArticle() {}
+export default function MainArticle() {
+  return (
+    <section className='main-article'>
+      <img src='' alt='' className='main-article__hero-image' />
+
+      <div className='main-article__bottom-section'>
+        <h1 className='main-article__heading'>The Bright Future of Web 3.0?</h1>
+
+        <div className='main-article__container'>
+          <p className='main-article__description'>
+            We dive into the next evolution of the web that claims to put the
+            power of the platforms back into the hands of the people. But is it
+            really fulfilling its promise?
+          </p>
+          <button className='main-article__button'>Read More</button>
+        </div>
+      </div>
+    </section>
+  );
+}
