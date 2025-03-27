39cd8c3229cc116ea01d1879c5ceff5ad7ca04d9
Venus <venusyip@outlook.com>
2025-03-27 22:06:55 +0000

    Add markup for nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,1 +1,38 @@
-export default function NavBar() {}
+export default function NavBar() {
+  return (
+    <header className='nav-bar'>
+      <a href='#' className='nav-bar__logo-button'>
+        <img
+          src={require('../assets/images/logo.svg')}
+          alt='Logo'
+          className='nav-bar__logo'
+        />
+      </a>
+
+      <button className='nav-bar__menu-button'>
+        <img
+          src={require('../assets/images/icon-menu.svg')}
+          alt='Menu button'
+        />
+      </button>
+
+      <nav className='pages'>
+        <a href='#' className='pages__link'>
+          Home
+        </a>
+        <a href='#' className='pages__link'>
+          New
+        </a>
+        <a href='#' className='pages__link'>
+          Popular
+        </a>
+        <a href='#' className='pages__link'>
+          Trending
+        </a>
+        <a href='#' className='pages__link'>
+          Categories
+        </a>
+      </nav>
+    </header>
+  );
+}
