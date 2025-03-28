0320d0bd38ed369cf7e0c367de0890dac3422e64
Venus <venusyip@outlook.com>
2025-03-28 14:52:24 +0000

    Add text content for new articles

--- src/components/NewArticles.jsx
+++ src/components/NewArticles.jsx
@@ -4,18 +4,32 @@ export default function NewArticles() {
       <h2 className='new-articles__main-heading'>New</h2>
 
       <article className='new-articles__article'>
-        <h3 className='new-articles__article-heading'></h3>
-        <p className='new-articles__description'></p>
+        <h3 className='new-articles__article-heading'>
+          Hydrogen VS Electric Cars
+        </h3>
+        <p className='new-articles__description'>
+          Will hydrogen-fueled cars ever catch up to EVs?
+        </p>
       </article>
 
       <article className='new-articles__article'>
-        <h3 className='new-articles__article-heading'></h3>
-        <p className='new-articles__description'></p>
+        <h3 className='new-articles__article-heading'>
+          The Downsides of AI Artistry
+        </h3>
+        <p className='new-articles__description'>
+          What are the possible adverse effects of on-demand AI image
+          generation?
+        </p>
       </article>
 
       <article className='new-articles__article'>
-        <h3 className='new-articles__article-heading'></h3>
-        <p className='new-articles__description'></p>
+        <h3 className='new-articles__article-heading'>
+          Is VC Funding Drying Up?
+        </h3>
+        <p className='new-articles__description'>
+          Private funding by VC firms is down 50% YOY. We take a look at what
+          that means.
+        </p>
       </article>
     </section>
   );
